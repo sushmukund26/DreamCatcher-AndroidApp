edd703a88dea960fdb962c30006b418ac3ca70ee
Sush Mukund <msushmethaa@vt.edu>
2020-04-09 14:21:45 -0400

    Adds support for swipe to delete

--- app/src/main/java/edu/vt/cs/cs5254/dreamcatcher/DreamDetailFragment.kt
+++ app/src/main/java/edu/vt/cs/cs5254/dreamcatcher/DreamDetailFragment.kt
@@ -16,6 +16,7 @@ import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -81,6 +82,9 @@ class DreamDetailFragment : Fragment(), AddDreamEntryFragment.Callbacks {
         dreamEntryRecyclerView =
             view.findViewById(R.id.dream_entry_recycler_view) as RecyclerView
         dreamEntryRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        val itemTouchHelper = ItemTouchHelper(SwipeToDeleteCallback())
+        itemTouchHelper.attachToRecyclerView(dreamEntryRecyclerView)
 
         photoView = view.findViewById(R.id.dream_photo) as ImageView
         addDreamEntryButton = view.findViewById(R.id.add_comment_fab)
@@ -359,6 +363,27 @@ class DreamDetailFragment : Fragment(), AddDreamEntryFragment.Callbacks {
         override fun onBindViewHolder(holder: DreamEntryHolder, position: Int) {
             val dreamEntry = dreamEntries[position]
             holder.bind(dreamEntry)
+        }
+
+        fun deleteItem(position: Int) {
+            val dreamEntryToDelete= dreamEntries[position]
+            dreamEntries = dreamEntries - dreamEntryToDelete
+            notifyItemRemoved(position)
+        }
+    }
+
+    inner class SwipeToDeleteCallback: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return false
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            adapter?.deleteItem(position)
         }
     }
 }
